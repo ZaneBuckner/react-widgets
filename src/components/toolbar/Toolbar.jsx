80abce26fccecaac11f1006fa76244986a57cf26
zanebuckner <bucknerzane@gmail.com>
2022-01-08 21:42:27 -0600

    Remove declarations that context provides
    The implemented WidgetContext now defines the widgets data object.
    All icon imports removed for the same reason above.

--- src/components/toolbar/Toolbar.jsx
+++ src/components/toolbar/Toolbar.jsx
@@ -1,72 +1,39 @@
-import { v4 as uuidv4 } from 'uuid';
+import { useContext } from 'react';
+import { WidgetContext } from 'context/WidgetContext';
 
-import { Container } from '../../globalStyles';
-import { StyledToolbar } from './Toolbar.styled';
-import WidgetWrapper from './WidgetWrapper';
-
-import CounterIcon from '../../Assets/CounterIcon';
-import { FaRegAddressCard as UserProfileIcon } from 'react-icons/fa';
-import { TiWeatherStormy as WeatherIcon } from 'react-icons/ti';
-import CodewarsIcon from '../../Assets/CodewarsIcon';
-import { MdTimer as TimerIcon } from 'react-icons/md';
-import { BsCalendarDate as CalendarIcon } from 'react-icons/bs';
-import { BsClock as ClockIcon } from 'react-icons/bs';
-// import { MdOutlineChecklistRtl as TodoIcon } from 'react-icons/md';
-// import { AiOutlineFontColors as FontIcon } from 'react-icons/ai';
-// import { AiOutlineBgColors as ColorIcon } from 'react-icons/ai';
-
-const widgets = [
-	{
-		name: 'Calendar',
-		icon: <CalendarIcon fill='#C3C3C3' />,
-	},
-	{
-		name: 'Clock',
-		icon: <ClockIcon fill='#C3C3C3' />,
-	},
-	{
-		name: 'CodeWars',
-		icon: <CodewarsIcon fill='#C3C3C3' />,
-	},
-	{
-		name: 'Counter',
-		icon: <CounterIcon fill='#C3C3C3' />,
-	},
-	{
-		name: 'Timer',
-		icon: <TimerIcon fill='#C3C3C3' />,
-	},
-	{
-		name: 'User Profile',
-		icon: <UserProfileIcon fill='#C3C3C3' />,
-	},
-	{
-		name: 'Weather',
-		icon: <WeatherIcon fill='#C3C3C3' />,
-	},
-	// {
-	// 	name: 'To-Do',
-	// 	icon: <TodoIcon fill='#C3C3C3' />,
-	// },
-	// {
-	// 	name: 'Color Editor',
-	// 	icon: <ColorIcon fill='#C3C3C3' />,
-	// },
-	// {
-	// 	name: 'Font Editor',
-	// 	icon: <FontIcon fill='#C3C3C3' />,
-	// },
-];
+import Button from 'components/shared/Button';
+import { BsChevronCompactLeft as NavLeft, BsChevronCompactRight as NavRight } from 'react-icons/bs';
+import {
+	StyledContainer,
+	StyledToolbarNav,
+	StyledToolbar,
+	StyledWidgetWrapper,
+} from './Toolbar.styled';
 
 function Toolbar() {
+	const [widgets, setWidgets] = useContext(WidgetContext);
+
+	const handleWidgetClick = widget => {
+		console.log(widget);
+	};
+
 	return (
-		<Container>
+		<StyledContainer>
+			<StyledToolbarNav>
+				<NavLeft className='nav-left' />
+			</StyledToolbarNav>
 			<StyledToolbar>
 				{widgets.map(widget => (
-					<WidgetWrapper key={uuidv4()} name={widget.name} icon={widget.icon}></WidgetWrapper>
+					<StyledWidgetWrapper key={widget.id}>
+						<Button onClick={() => handleWidgetClick(widget)}>{widget.icon}</Button>
+						<h1>{widget.name}</h1>
+					</StyledWidgetWrapper>
 				))}
 			</StyledToolbar>
-		</Container>
+			<StyledToolbarNav>
+				<NavRight className='nav-right' />
+			</StyledToolbarNav>
+		</StyledContainer>
 	);
 }
 
