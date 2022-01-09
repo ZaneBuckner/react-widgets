8d5b7b15c11954c88774de92371e2f41a74507c4
zanebuckner <bucknerzane@gmail.com>
2022-01-08 21:04:05 -0600

    Refactor header component
    The Nav component was redundant and removed. Its refs and properties
    were rewritten inside of Header as mapped function for readability.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,45 @@
+import Button from 'components/shared/Button';
+import Logo from 'components/logo/Logo';
+import LogoIcon from 'components/logo/LogoIcon';
+
 import { Container } from '../../globalStyles';
-import { StyledHeader } from './Header.styled';
-import Logo from '../logo/Logo';
-import Navigation from './Nav';
+import { StyledHeader, StyledNav } from './Header.styled';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+
+const mappedNavLinks = () => {
+	const links = [
+		{
+			title: 'Website',
+			url: 'https://zaniac.io/',
+			icon: <LogoIcon />,
+		},
+		{
+			title: 'GitHub Profile',
+			url: 'https://github.com/ZaneBuckner/',
+			icon: <FaGithubSquare />,
+		},
+		{
+			title: 'LinkedIn Profile',
+			url: 'https://www.linkedin.com/in/ZaneBuckner/',
+			icon: <FaLinkedin />,
+		},
+	];
+
+	return links.map(link => (
+		<Button>
+			<a title={link.title} href={link.url} target='_blank' rel='noopener noreferrer'>
+				{link.icon}
+			</a>
+		</Button>
+	));
+};
 
 function Header() {
 	return (
 		<Container header>
 			<StyledHeader>
 				<Logo strokeWidth={3} height={50} />
-				<Navigation />
+				<StyledNav>{mappedNavLinks()}</StyledNav>
 			</StyledHeader>
 		</Container>
 	);
