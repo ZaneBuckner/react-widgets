cafaea34d07272585eec562eea5a4bd4904388e8
zanebuckner <bucknerzane@gmail.com>
2022-01-08 22:17:43 -0600

    Fix mapping error
    Fixed error that was not resolved by providing item id.
    Hard coded the values instead.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,40 +6,38 @@ import { Container } from '../../globalStyles';
 import { StyledHeader, StyledNav } from './Header.styled';
 import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 
-const mappedNavLinks = () => {
-	const links = [
-		{
-			title: 'Website',
-			url: 'https://zaniac.io/',
-			icon: <LogoIcon />,
-		},
-		{
-			title: 'GitHub Profile',
-			url: 'https://github.com/ZaneBuckner/',
-			icon: <FaGithubSquare />,
-		},
-		{
-			title: 'LinkedIn Profile',
-			url: 'https://www.linkedin.com/in/ZaneBuckner/',
-			icon: <FaLinkedin />,
-		},
-	];
-
-	return links.map(link => (
-		<Button>
-			<a title={link.title} href={link.url} target='_blank' rel='noopener noreferrer'>
-				{link.icon}
-			</a>
-		</Button>
-	));
-};
-
 function Header() {
 	return (
 		<Container header>
 			<StyledHeader>
 				<Logo strokeWidth={3} height={50} />
-				<StyledNav>{mappedNavLinks()}</StyledNav>
+				<StyledNav>
+					<Button>
+						<a title='Website' href='https://zaniac.io/' target='_blank' rel='noopener noreferrer'>
+							<LogoIcon />
+						</a>
+					</Button>
+					<Button>
+						<a
+							title='GitHub Profile'
+							href='https://github.com/ZaneBuckner/'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<FaGithubSquare />
+						</a>
+					</Button>
+					<Button>
+						<a
+							title='LinkedIn Profile'
+							href='https://www.linkedin.com/in/ZaneBuckner/'
+							target='_blank'
+							rel='noopener noreferrer'
+						>
+							<FaLinkedin />
+						</a>
+					</Button>
+				</StyledNav>
 			</StyledHeader>
 		</Container>
 	);
