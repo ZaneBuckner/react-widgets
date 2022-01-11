1f63d1d4f30840ec2da9b29adb23ebe598213ec0
zanebuckner <bucknerzane@gmail.com>
2022-01-10 21:15:32 -0600

    Add functionality to show or remove widget
    Included method to check a specific widget's display state,
    and display widget if that value is true. The solution is working
    as intended, but I am getting a warning from react when the
    clock widget is toggled (Cannot perform update on unmounted...).

--- src/components/widgets/Widgets.jsx
+++ src/components/widgets/Widgets.jsx
@@ -1,4 +1,7 @@
-import { Container } from '../../globalStyles';
+import { useContext } from 'react';
+import { WidgetContext } from 'context/WidgetContext';
+
+import { Container } from 'globalStyles';
 import { Grid } from '@material-ui/core';
 
 import Counter from './counter/Counter';
@@ -6,18 +9,6 @@ import User from './user/User';
 import Weather from './weather/Weather';
 import Codewars from './codewars/Codewars';
 import Clock from './clock/Clock';
-
-// const useStyles = makeStyles(theme => ({
-// 	root: {},
-// 	paper: {
-// 		// padding: theme.spacing(2),
-// 		// textAlign: 'center',
-// 		// color: theme.palette.text.secondary,
-// 		padding: '1.5rem',
-// 		borderRadius: '10px',
-// 		boxShadow: '2px 6px 13px -4px rgba(0, 0, 0, 0.57)',
-// 	},
-// }));
 
 const widgetGridStyles = {
 	alignItems: 'center',
@@ -28,24 +19,36 @@ const widgetGridStyles = {
 };
 
 function Widgets() {
+	const { currentDisplay } = useContext(WidgetContext);
+
 	return (
-		<Container>
+		<Container widgets>
 			<Grid container spacing={3} style={widgetGridStyles}>
-				<Grid item xs={12} sm={6} md={4}>
-					<Weather />
-				</Grid>
-				<Grid item xs={12} sm={6} md={4}>
-					<Clock />
-				</Grid>
-				<Grid item xs={12} sm={6} md={4}>
-					<Counter />
-				</Grid>
-				<Grid item xs={12} sm={12} md={12}>
-					<Codewars />
-				</Grid>
-				<Grid item xs={12} sm={12} md={6}>
-					<User />
-				</Grid>
+				{currentDisplay('weather') && (
+					<Grid item xs={12} sm={6} md={4}>
+						<Weather />
+					</Grid>
+				)}
+				{currentDisplay('clock') && (
+					<Grid item xs={12} sm={6} md={4}>
+						<Clock />
+					</Grid>
+				)}
+				{currentDisplay('codewars') && (
+					<Grid item xs={12} sm={12} md={12}>
+						<Codewars />
+					</Grid>
+				)}
+				{currentDisplay('counter') && (
+					<Grid item xs={12} sm={6} md={4}>
+						<Counter />
+					</Grid>
+				)}
+				{currentDisplay('userprofile') && (
+					<Grid item xs={12} sm={12} md={6}>
+						<User />
+					</Grid>
+				)}
 			</Grid>
 		</Container>
 	);
