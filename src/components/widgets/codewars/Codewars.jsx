eeb5ef6ecd5876de6d965c880ed2676ad4a8e278
zanebuckner <bucknerzane@gmail.com>
2022-01-12 19:45:37 -0600

    Refactor to use cardHeader features
    The cardHeader component was updated to own the search input, modal,
    and close widget functionality. The Codewars component was refactored to
    use said functionality.

--- src/components/widgets/codewars/Codewars.jsx
+++ src/components/widgets/codewars/Codewars.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react';
-
 import Modal from 'components/shared/Modal';
 import CodewarsModal from './CodewarsModal';
 import UserProfile from './UserProfile';
@@ -10,37 +9,31 @@ import Card from 'components/shared/Card';
 import CardHeader from 'components/shared/CardHeader';
 import { StyledDashboard } from './Codewars.styled';
 import CodewarsIcon from 'Assets/CodewarsIcon';
-import InfoIcon from '@material-ui/icons/Info';
-import CancelIcon from '@material-ui/icons/Cancel';
 
 function Codewars() {
-	const [user, setUser] = useState('Zaniac');
+	const [userInput, setUserInput] = useState('Zaniac');
 	const [showModal, setShowModal] = useState(false);
-	// const [closeWidget, setCloseWidget] = useState(false);
 	const [selectedChallenge, setSelectedChallenge] = useState(null);
-
-	const handleModalState = () => setShowModal(prev => !prev);
-
-	const handleCloseState = () => console.log('Widget Closed');
-
-	const searchUser = e => e.key === 'Enter' && setUser(e.target.value);
 
 	const challegeDetails = <ChallengeDetails selectedChallenge={selectedChallenge} />;
 	const awaitChallengeDetails = <p className='user-message'>Select a challenge for details.</p>;
 
 	return (
 		<Card>
-			<CardHeader widgetName='Codewars Dashboard' icon={<CodewarsIcon />}>
-				<input type='text' placeholder='Search User' spellCheck='false' onKeyPress={searchUser} />
-				<InfoIcon className='header-icon' onClick={handleModalState} />
-				<CancelIcon className='header-icon' onClick={handleCloseState} />
-			</CardHeader>
+			<CardHeader
+				name='Codewars'
+				icon={<CodewarsIcon />}
+				placeholder='Search User'
+				widgetRef='codewars'
+				setUserInput={setUserInput}
+				setShowModal={setShowModal}
+			/>
 			<Modal showModal={showModal} setShowModal={setShowModal}>
 				<CodewarsModal />
 			</Modal>
 			<StyledDashboard>
-				<UserProfile user={user} />
-				<ChallengesList user={user} setSelectedChallenge={setSelectedChallenge} />
+				<UserProfile user={userInput} />
+				<ChallengesList user={userInput} setSelectedChallenge={setSelectedChallenge} />
 				{selectedChallenge ? challegeDetails : awaitChallengeDetails}
 			</StyledDashboard>
 		</Card>
