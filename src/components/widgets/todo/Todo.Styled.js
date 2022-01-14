899e1b06d19824ae08b62874dec52c24b2c31662
zanebuckner <bucknerzane@gmail.com>
2022-01-13 18:29:31 -0600

    Add input element styling for Todo component

--- src/components/widgets/todo/Todo.Styled.js
+++ src/components/widgets/todo/Todo.Styled.js
@@ -4,3 +4,21 @@ export const StyledTodo = styled.div`
 	/* border: 0.1px solid lightblue; */
 	height: 19rem;
 `;
+
+export const StyledInput = styled.input`
+	display: flex;
+	align-items: center;
+	padding: 0.2rem 0.5rem;
+
+	background-color: rgba(255, 255, 255, 0.1);
+	border-radius: 5px;
+
+	font-family: 'Roboto', serif;
+	color: #c3c3c3;
+
+	::placeholder {
+		font: inherit;
+		color: #c3c3c3;
+		opacity: 80%;
+	}
+`;
