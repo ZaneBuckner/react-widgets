114bcdd050312270b0b610925cf45a7cae72b66c
zanebuckner <bucknerzane@gmail.com>
2022-01-14 22:54:29 -0600

    Update card header component for dynamic activity
    I separated the input, modal, and close actions into a separate div
    to fix the actions on the right side of the card. Previously, if a
    widget didn't 'subscribe' for an input, the styling was destroyed.
    - Gave some elements classes for readability.

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -3,7 +3,14 @@ import styled from 'styled-components';
 
 const StyledCard = styled.div`
 	position: relative;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100%;
 	padding: 1rem;
+
 	background-color: #353535;
 	border-radius: 10px;
 	box-shadow: 2px 6px 13px -4px rgba(0, 0, 0, 0.57);
--- src/components/shared/CardHeader.jsx
+++ src/components/shared/CardHeader.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { WidgetContext } from 'context/WidgetContext';
+import { motion, AnimatePresence } from 'framer-motion';
 import styled from 'styled-components';
 
 import InfoIcon from '@material-ui/icons/Info';
@@ -21,45 +22,51 @@ const StyledCardHeader = styled.div`
 		fill: #dab55d;
 	}
 
-	h1 {
+	.widget-title {
 		margin-left: 0.5rem;
 		font-size: 0.8rem;
 		font-weight: 400;
 		color: #dab55d;
+
+		@media (max-width: 350px) {
+			display: none;
+		}
 	}
 
-	input {
+	@media (max-width: 500px) {
+		height: 1.8rem;
+	}
+`;
+
+const StyledCardHeaderActions = styled.div`
+	display: flex;
+	justify-content: center;
+	height: 100%;
+	margin-left: auto;
+
+	.action-input {
 		display: flex;
 		align-items: center;
 		padding: 0.2rem 0.5rem;
 		margin-left: auto;
-		width: 40%;
-
-		background-color: rgba(255, 255, 255, 0.1);
-		border-radius: 5px;
+		max-width: 6.5rem;
 
 		font-family: 'Roboto', serif;
 		color: #c3c3c3;
 
+		background-color: rgba(255, 255, 255, 0.1);
+		border-radius: 5px;
+
 		::placeholder {
 			font: inherit;
 			color: #c3c3c3;
-			opacity: 80%;
 		}
 	}
 
-	.header-icons {
-		height: 100%;
-		margin-left: 0.3rem;
+	.action-icons {
 		cursor: pointer;
-
-		@media (max-width: 500px) {
-			height: 150%;
-		}
-	}
-
-	.info {
-		margin-left: auto;
+		height: 100%;
+		margin-left: 0.2rem;
 	}
 `;
 
@@ -70,22 +77,33 @@ function CardHeader({ name, icon, placeholder, widgetRef, setUserInput, setShowM
 	return (
 		<StyledCardHeader>
 			{icon}
-			<h1>{name}</h1>
-			{setUserInput && (
-				<input type='text' placeholder={placeholder} spellCheck='false' onKeyPress={userInput} />
-			)}
-			{setShowModal && (
-				<InfoIcon
-					className='header-icons info'
-					aria-label='Open Widget Modal'
-					onClick={() => setShowModal(prev => !prev)}
+			<h1 className='widget-title'>{name}</h1>
+			<StyledCardHeaderActions>
+				{/* OPTIONAL: GIVES WIDGET SEARCH INPUT */}
+				{setUserInput && (
+					<input
+						className='action-input'
+						type='text'
+						placeholder={placeholder}
+						spellCheck='false'
+						onKeyPress={userInput}
+					/>
+				)}
+				{/* OPTIONAL: GIVES WIDGET A POPUP MODAL */}
+				{setShowModal && (
+					<InfoIcon
+						className='action-icons'
+						aria-label='Open Widget Modal'
+						onClick={() => setShowModal(prev => !prev)}
+					/>
+				)}
+				{/* REQUIRED: CLOSES WIDGET */}
+				<CancelIcon
+					className='action-icons'
+					aria-label='Close Widget'
+					onClick={() => toggleDisplay(widgetRef)}
 				/>
-			)}
-			<CancelIcon
-				className='header-icons close'
-				aria-label='Close Widget'
-				onClick={() => toggleDisplay(widgetRef)}
-			/>
+			</StyledCardHeaderActions>
 		</StyledCardHeader>
 	);
 }
