c67c953886c5a818cfca1e5949c8b88051ca7af1
zanebuckner <bucknerzane@gmail.com>
2022-01-14 21:55:34 -0600

    Refactor codewars challenge list
    Defined the mapped list in return instead of a function reference,
    seemed unnecessary.

--- src/components/widgets/codewars/ChallengesList.jsx
+++ src/components/widgets/codewars/ChallengesList.jsx
@@ -5,15 +5,6 @@ function ChallengesList({ user, setSelectedChallenge }) {
 	const url = `https://www.codewars.com/api/v1/users/${user}/code-challenges/completed/`;
 	const { data, loading, error } = useAxios(url);
 
-	const mappedList = () => {
-		const challenges = data.data;
-		return challenges.map(challenge => (
-			<li key={challenge.id} onClick={() => setSelectedChallenge(challenge)}>
-				{challenge.name}
-			</li>
-		));
-	};
-
 	const preRender = {
 		loading: <p className='user-message'>Loading Challenges...</p>,
 		error: <p className='user-message'>Unable to find completed challenges.</p>,
@@ -21,7 +12,15 @@ function ChallengesList({ user, setSelectedChallenge }) {
 
 	if (loading) return preRender.loading;
 	if (error) return preRender.error;
-	return <StyledChallengesList>{mappedList()}</StyledChallengesList>;
+	return (
+		<StyledChallengesList>
+			{data.data.map(challenge => (
+				<li key={challenge.id} onClick={() => setSelectedChallenge(challenge)}>
+					{challenge.name}
+				</li>
+			))}
+		</StyledChallengesList>
+	);
 }
 
 export default ChallengesList;
