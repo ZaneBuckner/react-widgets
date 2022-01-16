a2c165dc03f18ef1834743c36bb52808f1ee1e03
zanebuckner <bucknerzane@gmail.com>
2022-01-16 00:24:18 -0600

    Update Task to return delete and drag methods
    Sends back target ID when either event is invoked.

--- src/components/widgets/todo/Task.jsx
+++ src/components/widgets/todo/Task.jsx
@@ -1,15 +1,19 @@
 import { StyledTask } from './Todo.Styled';
 
 import { RiDeleteBinLine as DeleteIcon } from 'react-icons/ri';
+import { GoThreeBars as DragIcon } from 'react-icons/go';
 
-function Task({ task }) {
+function Task({ task, onDelete, onToggle }) {
 	return (
-		<StyledTask>
+		<StyledTask onDoubleClick={() => onToggle(task.id)}>
 			<div className='info-wrapper'>
 				<h1 className='title'>{task.title}</h1>
 				<h2 className='date'>{task.date}</h2>
 			</div>
-			<DeleteIcon />
+			<div className='actions-wrapper'>
+				<DeleteIcon onClick={() => onDelete(task.id)} />
+				<DragIcon />
+			</div>
 		</StyledTask>
 	);
 }
