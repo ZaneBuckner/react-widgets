01eca5f293cd0d766a504025571a7dcae511ee0e
zanebuckner <bucknerzane@gmail.com>
2022-01-16 00:19:42 -0600

    Add global functionality
    - Set up state storage
    - Added method for deleting a task, deleteTask()
    - Added method for toggling a reminder, toggleReminder()

--- src/components/widgets/todo/Todo.jsx
+++ src/components/widgets/todo/Todo.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 import Card from 'components/shared/Card';
@@ -8,33 +9,56 @@ import { StyledTodo } from './Todo.Styled';
 
 import { MdOutlineChecklistRtl as TodoIcon } from 'react-icons/md';
 
-const tasks = [
-	{
-		id: uuidv4(),
-		title: 'Task 1',
-		date: 'MMM DDth @ 9:30am',
-		reminder: false,
-	},
-	{
-		id: uuidv4(),
-		title: 'Task 2',
-		date: 'MMM DDth @ 4:20pm',
-		reminder: false,
-	},
-	{
-		id: uuidv4(),
-		title: 'Task 3',
-		date: 'MMM DDth @ 1:30pm',
-		reminder: false,
-	},
-];
-
 function Todo() {
+	const [tasks, setTasks] = useState([
+		{
+			id: uuidv4(),
+			title: 'Task 1',
+			date: 'MMM DDth @ 9:30am',
+			reminder: false,
+		},
+		{
+			id: uuidv4(),
+			title: 'Task 2',
+			date: 'MMM DDth @ 4:20pm',
+			reminder: false,
+		},
+		{
+			id: uuidv4(),
+			title: 'Task 3',
+			date: 'MMM DDth @ 1:30pm',
+			reminder: false,
+		},
+	]);
+
+	const deleteTask = id => {
+		const filteredTasks = tasks.filter(task => task.id !== id);
+		setTasks(filteredTasks);
+	};
+
+	const toggleReminder = id => {
+		const updatedTasks = tasks.map(task => {
+			if (task.id !== id) return task;
+			return {
+				...task,
+				reminder: !task.reminder,
+			};
+		});
+
+		setTasks(updatedTasks);
+	};
+
+	const messageTasksCompleted = <h1 className='user-message'>All Tasks Completed</h1>;
+
 	return (
 		<Card>
 			<CardHeader name='Todo Tracker' icon={<TodoIcon />} widgetRef='todo' />
 			<StyledTodo>
-				<TaskList tasks={tasks} />
+				{tasks.length > 0 ? (
+					<TaskList tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+				) : (
+					messageTasksCompleted
+				)}
 			</StyledTodo>
 		</Card>
 	);
