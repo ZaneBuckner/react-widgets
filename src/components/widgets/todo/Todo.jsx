6b62aa19fafc362f9f4ebdb8eaf31b2f5ad0c625
zanebuckner <bucknerzane@gmail.com>
2022-01-19 00:48:09 -0600

    Add utility modal functionality to task widget
    - Set and supplied content for UtilityModal, the form to add new tasks.
    - Supplied content for informational Modal.

--- src/components/widgets/todo/Todo.jsx
+++ src/components/widgets/todo/Todo.jsx
@@ -3,14 +3,19 @@ import { v4 as uuidv4 } from 'uuid';
 
 import Card from 'components/shared/Card';
 import CardHeader from 'components/shared/CardHeader';
+import Modal from 'components/shared/Modal';
+import UtilityModal from 'components/shared/UtilityModal';
 import AddTask from './AddTask';
 import TaskList from './TaskList';
 
 import { StyledTodo } from './Todo.Styled';
 
 import { MdOutlineChecklistRtl as TodoIcon } from 'react-icons/md';
+import { MdPlaylistAdd as AddElementIcon } from 'react-icons/md';
 
 function Todo() {
+	const [showModal, setShowModal] = useState(false);
+	const [showUtilityModal, setShowUtilityModal] = useState(false);
 	const [tasks, setTasks] = useState([
 		{
 			id: uuidv4(),
@@ -35,7 +40,6 @@ function Todo() {
 	const addTask = newTask => {
 		const addNewTask = { ...newTask, id: uuidv4() };
 		setTasks([...tasks, addNewTask]);
-		console.log(tasks);
 	};
 
 	const deleteTask = id => {
@@ -56,9 +60,36 @@ function Todo() {
 
 	return (
 		<Card>
-			<CardHeader name='Todo Tracker' icon={<TodoIcon />} widgetRef='todo' />
-			<StyledTodo>
+			<CardHeader
+				name='Todo Tracker'
+				icon={<TodoIcon />}
+				widgetRef='todo'
+				setShowModal={setShowModal}
+				utilityModal={
+					<AddElementIcon
+						title='Add New Task'
+						className='action-icons'
+						aria-label='Open Widget Modal'
+						onClick={() => setShowUtilityModal(!showUtilityModal)}
+					/>
+				}
+			/>
+			<Modal showModal={showModal} setShowModal={setShowModal}>
+				<TodoIcon />
+				<h1 className='modal-title'>Todo Tracker</h1>
+				<h2 className='modal-description'>
+					Take control and organize your day-to-day tasks with this efficient Task Tracker.
+				</h2>
+				<p className='modal-usage'>
+					To add a task, select the {<AddElementIcon />} icon.
+					<br />
+					Only a Task Name is required.
+				</p>
+			</Modal>
+			<UtilityModal showUtilityModal={showUtilityModal} setShowUtilityModal={setShowUtilityModal}>
 				<AddTask onAdd={addTask} />
+			</UtilityModal>
+			<StyledTodo>
 				{tasks.length > 0 ? (
 					<TaskList tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 				) : (
