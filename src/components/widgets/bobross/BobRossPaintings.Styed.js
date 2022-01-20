5f30b3980f6c4327e37f684a2d5b093d282aa6ff
zanebuckner <bucknerzane@gmail.com>
2022-01-20 00:14:11 -0600

    Update bobross painting component
    - Imported painting frame png image.
    - Setup initial logic for getting a painting, alternative method
    in the works (to get painting name).
    - Updated styling for frame and image.

--- src/components/widgets/bobross/BobRossPaintings.Styed.js
+++ src/components/widgets/bobross/BobRossPaintings.Styed.js
@@ -9,4 +9,23 @@ export const StyledBobRossPaintings = styled.div`
 
 	font-family: 'Roboto', serif;
 	color: #c3c3c3;
+
+	.painting-wrapper {
+		position: relative;
+		width: 100%;
+		height: auto;
+		background-repeat: no-repeat;
+		background-size: contain;
+
+		.painting-frame {
+			position: absolute;
+			width: 100%;
+			height: 100%;
+		}
+
+		.painting-image {
+			width: 100%;
+			height: 100%;
+		}
+	}
 `;
--- src/components/widgets/bobross/BobRossPaintings.jsx
+++ src/components/widgets/bobross/BobRossPaintings.jsx
@@ -1,16 +1,26 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import Card from 'components/shared/Card';
 import CardHeader from 'components/shared/CardHeader';
 import Modal from 'components/shared/Modal';
 import { StyledBobRossPaintings } from './BobRossPaintings.Styed';
+import bronzeFrame from 'Assets/Images/bronze-frame.png';
 
 import BobRossIcon from 'Assets/BobRossIcon';
 import { BiRefresh as RefreshIcon } from 'react-icons/bi';
 
 function BobRossPaintings() {
+	const [painting, setPainting] = useState('');
 	const [showModal, setShowModal] = useState(false);
-	const [showUtilityModal, setShowUtilityModal] = useState(false);
+
+	const getPaintingURL = () => {
+		let rand = Math.floor(Math.random() * 282);
+		setPainting(`https://www.twoinchbrush.com/images/painting${rand}.png`);
+	};
+
+	useEffect(() => {
+		getPaintingURL();
+	}, []);
 
 	return (
 		<Card>
@@ -24,7 +34,7 @@ function BobRossPaintings() {
 						title='New Painting'
 						className='action-icons'
 						aria-label='Open Widget Modal'
-						onClick={() => setShowUtilityModal(!showUtilityModal)}
+						onClick={() => getPaintingURL()}
 					/>
 				}
 			/>
@@ -37,7 +47,10 @@ function BobRossPaintings() {
 				<p className='modal-usage'>For a new painting, select the {<RefreshIcon />} icon.</p>
 			</Modal>
 			<StyledBobRossPaintings>
-				<h1>Bob Ross Paintings</h1>
+				<div className='painting-wrapper'>
+					<img className='painting-frame' src={bronzeFrame} alt='' />
+					{painting && <img className='painting-image' src={painting} alt='' />}
+				</div>
 			</StyledBobRossPaintings>
 		</Card>
 	);
