4a1747925704d10715cc6fd57409b5d941fb142a
zanebuckner <bucknerzane@gmail.com>
2022-01-19 21:44:24 -0600

    Add modal and utility modal components
    - Add content for informational modal.
    - Planning on using the refresh icon to fetch new
    painting from API.

--- src/components/widgets/bobross/BobRossPaintings.jsx
+++ src/components/widgets/bobross/BobRossPaintings.jsx
@@ -6,9 +6,11 @@ import Modal from 'components/shared/Modal';
 import { StyledBobRossPaintings } from './BobRossPaintings.Styed';
 
 import BobRossIcon from 'Assets/BobRossIcon';
+import { BiRefresh as RefreshIcon } from 'react-icons/bi';
 
 function BobRossPaintings() {
 	const [showModal, setShowModal] = useState(false);
+	const [showUtilityModal, setShowUtilityModal] = useState(false);
 
 	return (
 		<Card>
@@ -17,7 +19,23 @@ function BobRossPaintings() {
 				icon={<BobRossIcon />}
 				widgetRef='bobross'
 				setShowModal={setShowModal}
+				utilityModal={
+					<RefreshIcon
+						title='New Painting'
+						className='action-icons'
+						aria-label='Open Widget Modal'
+						onClick={() => setShowUtilityModal(!showUtilityModal)}
+					/>
+				}
 			/>
+			<Modal showModal={showModal} setShowModal={setShowModal}>
+				<BobRossIcon width={25} height={25} fill={'#DAB55D'} />
+				<h1 className='modal-title'>Bob Ross Paintings</h1>
+				<h2 className='modal-description'>
+					"We don't make mistakes, just happy little accidents."
+				</h2>
+				<p className='modal-usage'>For a new painting, select the {<RefreshIcon />} icon.</p>
+			</Modal>
 			<StyledBobRossPaintings>
 				<h1>Bob Ross Paintings</h1>
 			</StyledBobRossPaintings>
