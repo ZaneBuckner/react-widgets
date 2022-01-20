e5892dbeaed655c69380e0cd40bc7134e51cf7e0
zanebuckner <bucknerzane@gmail.com>
2022-01-19 21:42:42 -0600

    Add initial component styling

---
+++ src/components/widgets/bobross/BobRossPaintings.Styed.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledBobRossPaintings = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	width: 100%;
+	height: 20rem;
+
+	font-family: 'Roboto', serif;
+	color: #c3c3c3;
+`;
