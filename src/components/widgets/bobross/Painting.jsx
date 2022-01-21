cd47f70331a4e512a4099c58b9359c042b80264c
zanebuckner <bucknerzane@gmail.com>
2022-01-20 23:34:24 -0600

    Create component that displays painting and frame

---
+++ src/components/widgets/bobross/Painting.jsx
@@ -0,0 +1,13 @@
+import { StyledPainting } from './BobRossPaintings.Styed';
+import tealFrame from 'Assets/Images/tealFrame.png';
+
+function Painting({ painting }) {
+	return (
+		<StyledPainting>
+			<img className='painting-frame' src={tealFrame} alt='Teal Frame' />
+			<img className='painting-image' src={painting.img_src} alt='Bob Ross Painting' />
+		</StyledPainting>
+	);
+}
+
+export default Painting;
