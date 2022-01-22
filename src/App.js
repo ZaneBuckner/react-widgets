6e39e0445473cee4dd817c8293a013e806a5ec04
zanebuckner <bucknerzane@gmail.com>
2022-01-21 22:56:36 -0600

    Add routing for multipage application
    My goal is to save the application's state per individual user, with
    user authentication. Whether local or session storage is better or not,
    I also want to learn how to talk with a backend, in my case, Firebase.
    These are my preliminary routes:
    - Home Page (not sure what its purpose is yet)
    - Widgets Dashboard
    - User Authentication (to login or register)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { WidgetContextProvider } from 'context/WidgetContext';
 
+import Home from 'components/home/Home';
+import AuthModal from 'components/shared/AuthModal';
 import Header from './components/header/Header';
 import Toolbar from './components/toolbar/Toolbar';
 import Widgets from './components/widgets/Widgets';
 
 function App() {
 	return (
-		<>
+		<Router>
 			<Header />
-			<WidgetContextProvider>
-				<Toolbar />
-				<Widgets />
-			</WidgetContextProvider>
-		</>
+			<Routes>
+				<Route exact path='/' element={<Home />} />
+				<Route
+					path='/widgets-dashboard'
+					element={
+						<WidgetContextProvider>
+							<Toolbar />
+							<Widgets />
+						</WidgetContextProvider>
+					}
+				/>
+				<Route path='/signup' element={<AuthModal />} />
+			</Routes>
+		</Router>
 	);
 }
 
