d5bb11abe1527d3e4a742e42789215a86ff4f0d2
zanebuckner <bucknerzane@gmail.com>
2022-01-25 16:15:01 -0600

    Update style height to the standard defined value
    Standard defined height for all widgets => 20rem => 300px.
    Remove unnessary styles.

--- src/components/widgets/counter/Counter.styled.js
+++ src/components/widgets/counter/Counter.styled.js
@@ -5,10 +5,7 @@ export const StyledCounter = styled.div`
 	justify-content: space-between;
 	align-items: center;
 	margin: auto;
-
-	/* Button {
-		width: 4rem;
-	} */
+	height: 20rem;
 `;
 
 export const CountDisplay = styled.h1`
@@ -18,6 +15,7 @@ export const CountDisplay = styled.h1`
 	font-size: 3rem;
 	text-align: center;
 
+	/* TEXT SHADOW EFFECT */
 	background-color: #161616;
 	color: transparent;
 	text-shadow: 2px 2px 3px #333333;
