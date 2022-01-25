4450599fabcca93e974890efb365602d2382be28
zanebuckner <bucknerzane@gmail.com>
2022-01-24 20:38:01 -0600

    Create footer component for application

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import { StyledFooterContainer, StyledFooterWrapper } from './Footer.Styled';
+
+function Footer() {
+	return (
+		<StyledFooterContainer className='footer'>
+			<StyledFooterWrapper>
+				<h1>FOOTER CONTENT</h1>
+			</StyledFooterWrapper>
+		</StyledFooterContainer>
+	);
+}
+
+export default Footer;
