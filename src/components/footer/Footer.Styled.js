cc5aca0efd59f6d0d990587eab60029edad5c241
zanebuckner <bucknerzane@gmail.com>
2022-01-24 20:38:40 -0600

    Create footer component styles

---
+++ src/components/footer/Footer.Styled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StyledFooterContainer = styled.div`
+	width: 100%;
+	height: 10rem;
+	margin-top: auto;
+
+	box-shadow: inset 0 6px 13px -4px rgb(0 0 0 / 48%);
+	background-color: #2b2b2b;
+
+	font-family: 'Montserrat', serif;
+`;
+
+export const StyledFooterWrapper = styled.div`
+	width: 100%;
+	max-width: 1200px;
+	height: 100%;
+	margin: 0 auto;
+	padding: 0.5rem;
+`;
