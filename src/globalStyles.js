1b7baea47913270db525255c1360e9ed74ad37e9
zanebuckner <bucknerzane@gmail.com>
2022-01-25 21:48:48 -0600

    Add container prop to vertical center cards

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
 	width: 100%;
 	max-width: ${({ header }) => (header ? 'none' : '1200px')};
-	margin: 0 auto;
+	margin: ${({ centerJustify }) => (centerJustify ? 'auto 0' : '0 auto')};
 	padding: ${({ header }) => (header ? '0 30px' : '0 50px')};
 
 	@media (max-width: 500px) {
