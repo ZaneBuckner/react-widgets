fcb16c894f01f5335f86716ea18820a75a88774d
zanebuckner <bucknerzane@gmail.com>
2022-01-25 23:50:10 -0600

    Add content for home page
    - Two buttons that can log in a user and create account.
    - Alternate sign in options, might change.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,28 +1,84 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import { Container } from 'globalStyles';
 import Card from 'components/shared/Card';
 import CardAnimation from 'components/shared/CardAnimation';
+import Button from 'components/shared/Button';
+import { StyledHomePage, StyledHeader, StyledContent } from './HomePage.Styled';
 
-const StyledHome = styled.div`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	width: 100%;
-	height: 100%;
-
-	font-family: 'Roboto', serif;
-	color: #c3c3c3;
-`;
+import {
+	BsGoogle as GoogleIcon,
+	BsGithub as GitHubIcon,
+	BsApple as AppleIcon,
+} from 'react-icons/bs';
 
 function HomePage() {
+	const getSignInMethods = () => {
+		let methods = [
+			{
+				id: uuidv4(),
+				ref: 'google',
+				// path: '/widgets-dashboard',
+				icon: <GoogleIcon />,
+			},
+			{
+				id: uuidv4(),
+				ref: 'github',
+				// path: '/login',
+				icon: <GitHubIcon />,
+			},
+			{
+				id: uuidv4(),
+				ref: 'apple',
+				// path: '/signin',
+				icon: <AppleIcon />,
+			},
+		];
+
+		return methods.map(method => (
+			<Button animate key={method.id}>
+				{method.icon}
+			</Button>
+		));
+	};
+
+	const redirectMessage = (
+		<div className='redirect'>
+			Not interested?
+			<Link to='/widgets-dashboard'>
+				<span> Continue to dashboard</span>
+			</Link>
+		</div>
+	);
+
 	return (
-		<Container>
+		<Container centerJustify>
 			<CardAnimation>
 				<Card>
-					<StyledHome>
-						<h1>Home Page</h1>
-					</StyledHome>
+					<StyledHomePage>
+						<StyledHeader>
+							<h1 className='title'>Welcome To React Widgets</h1>
+							<p className='message'>
+								React is a popular open source technology developed by Facebook to build complex
+								user interfaces. To practice using this technology I built an application full of
+								small applications - widgets.
+							</p>
+							<div className='links'>
+								<Button>Sign In</Button>
+								<Button>Create Account</Button>
+							</div>
+						</StyledHeader>
+
+						<StyledContent>
+							<div className='signin-methods'>
+								{getSignInMethods()}
+								<h2>Sign in with another account</h2>
+							</div>
+							{redirectMessage}
+						</StyledContent>
+					</StyledHomePage>
 				</Card>
 			</CardAnimation>
 		</Container>
