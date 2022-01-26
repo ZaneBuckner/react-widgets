7857ca8d82efcd88f74bbc3ae46bc8e3bba6026b
zanebuckner <bucknerzane@gmail.com>
2022-01-26 00:02:33 -0600

    Update app to include auth context provider
    - Context is working, trying to figure out how to hide my config
    object from the public.
    - Minor changes include component name changes.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { WidgetContextProvider } from 'context/WidgetContext';
 
-import Home from 'components/home/Home';
-import SignupModal from 'components/signup/Signup';
+import { AuthContextProvider } from 'context/AuthContext';
+import { WidgetContextProvider } from 'context/WidgetContext';
+import { useAuthContext } from 'context/AuthContext';
+
+// import SignupModal from 'components/auth/SignupModal';
+import HomePage from 'pages/HomePage';
+import LoginModal from 'components/auth/LoginModal';
 import Header from './components/header/Header';
 import Toolbar from './components/toolbar/Toolbar';
 import Widgets from './components/widgets/Widgets';
+import Footer from 'components/footer/Footer';
+
+// import { FaUserCircle as UserIcon } from 'react-icons/fa';
+// import { RiLogoutCircleRLine as LogOutIcon } from 'react-icons/ri';
 
 function App() {
+	const { loading } = useAuthContext();
+
 	return (
-		<Router>
-			<Header />
-			<Routes>
-				<Route exact path='/' element={<Home />} />
-				<Route
-					path='/widgets-dashboard'
-					element={
-						<WidgetContextProvider>
-							<Toolbar />
-							<Widgets />
-						</WidgetContextProvider>
-					}
-				/>
-				<Route path='/signup' element={<SignupModal />} />
-			</Routes>
-		</Router>
+		<AuthContextProvider>
+			<Router>
+				<Header />
+				<Routes>
+					<Route exact path='/' element={<HomePage />} />
+					<Route
+						path='/widgets-dashboard'
+						element={
+							<WidgetContextProvider>
+								<Toolbar />
+								<Widgets />
+							</WidgetContextProvider>
+						}
+					/>
+					{!loading && <Route path='/login' element={<LoginModal />} />}
+					{/* <Route path='/signup' element={<RegisterModal />} /> */}
+				</Routes>
+				<Footer />
+			</Router>
+		</AuthContextProvider>
 	);
 }
 
