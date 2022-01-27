cdd64ca0ecee78b655c771ced4dbe0d1d42cbeb6
zanebuckner <bucknerzane@gmail.com>
2022-01-27 17:31:05 -0600

    Create reusable input field component using MUI
    - Reusable inputField using MUI textField and the updated v5 styling
    engine that uses the styled-components library. The custom styling for
    the various states was odd, but I found A solution.
    - The hack solution was just playing around to see IF I could remove
    the browsers native ugly auto fill... it kind of works.

---
+++ src/components/shared/InputField.jsx
@@ -0,0 +1,99 @@
+import { styled } from '@mui/system';
+
+import { TextField } from '@mui/material';
+
+const StyledWrapper = styled('div')`
+	/* border: 0.1px solid lightblue; */
+	display: flex;
+	align-items: flex-end;
+	width: 100%;
+
+	font-family: 'Roboto', serif;
+	color: #c3c3c3;
+
+	svg {
+		margin-right: 1rem;
+		font-size: 1.5rem;
+	}
+
+	/* HACK: DISABLE INTERNAL AUTOFILL BACKGROUND-COLOR */
+	input:-webkit-autofill,
+	input:-webkit-autofill:hover,
+	input:-webkit-autofill:focus,
+	input:-webkit-autofill:active {
+		box-shadow: 0 0 0 30px #363636 inset !important;
+		-webkit-box-shadow: 0 0 0 30px #363636 inset !important;
+	}
+`;
+
+const color = {
+	gold: '#DAB55D',
+	grey: '#C3C3C3',
+	greyLight: '#838383',
+};
+
+const input = {
+	text: '.css-1480iag-MuiInputBase-root-MuiInput-root',
+	border: {
+		before: '.MuiInput-root:before',
+		hover: '.MuiInput-root:hover:not(.Mui-disabled):before',
+		focus: '.MuiInput-root:after',
+	},
+	label: {
+		before: '.MuiInputLabel-root',
+		focus: 'label.Mui-focused',
+	},
+};
+
+const StyledTextField = styled(TextField)`
+	width: 100%;
+
+	${input.text} {
+		color: ${color.grey};
+	}
+
+	${input.label.before} {
+		color: ${color.greyLight};
+	}
+	${input.label.focus} {
+		color: ${color.grey};
+	}
+
+	${input.border.before} {
+		border-color: ${color.greyLight};
+	}
+	${input.border.hover} {
+		border-color: ${color.grey};
+	}
+	${input.border.focus} {
+		border-color: ${color.gold};
+	}
+`;
+
+function InputField({
+	required,
+	showPassword,
+	updateShowPassword,
+	type,
+	title,
+	label,
+	icon,
+	...props
+}) {
+	return (
+		<StyledWrapper>
+			{icon}
+			<StyledTextField
+				required={required}
+				type={type}
+				title={title}
+				label={label}
+				icon={icon}
+				variant='standard'
+				{...props}
+			/>
+		</StyledWrapper>
+	);
+}
+
+export default InputField;
