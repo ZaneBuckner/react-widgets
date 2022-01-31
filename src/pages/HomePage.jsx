8e0fea05a5a6a34af0a7f65a858a140ae3ed03e9
zanebuckner <bucknerzane@gmail.com>
2022-01-30 21:15:21 -0600

    Add profile page to the home page component
    - The ProfilePage and HomePage exist if user is signed in or signed out.
    - Removed RouteLink component, not sure what I was thinking, not needed!

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
-import RouteLink from 'components/shared/RouteLink';
+import { useAuthContext } from 'context/AuthContext';
+import { Link } from 'react-router-dom';
 
 import { Container } from 'globalStyles';
 import Card from 'components/shared/Card';
@@ -6,27 +7,37 @@ import CardAnimation from 'components/shared/CardAnimation';
 
 import Button from 'components/shared/Button';
 import { StyledHomePage } from './Pages.Styled';
+import ProfilePage from './ProfilePage';
 
 function HomePage() {
+	const { currentUser } = useAuthContext();
+
+	const HomePage = (
+		<>
+			<h1 className='title'>Welcome To React Widgets</h1>
+			<p className='message'>
+				React is a popular open source technology developed by Facebook to build complex user
+				interfaces. To practice using this technology, I built an application full of small
+				applications - widgets.
+			</p>
+			<div className='links'>
+				<Link to='/login' children={<Button animate>Sign In</Button>} />
+				<Link to='/register' children={<Button animate>Create Account</Button>} />
+			</div>
+			<div className='redirect'>
+				Not interested?
+				<Link className='link' to='/widgets-dashboard' children=' Continue' />
+			</div>
+		</>
+	);
+
+	if (currentUser) return <ProfilePage />;
 	return (
 		<Container centerJustify>
 			<CardAnimation>
 				<Card pages>
 					<StyledHomePage className='StyledHomePage'>
-						<h1 className='title'>Welcome To React Widgets</h1>
-						<p className='message'>
-							React is a popular open source technology developed by Facebook to build complex user
-							interfaces. To practice using this technology I built an application full of small
-							applications - widgets.
-						</p>
-						<div className='links'>
-							<RouteLink to='/login' children={<Button>Sign In</Button>} />
-							<RouteLink to='/register' children={<Button>Create Account</Button>} />
-						</div>
-						<div className='redirect'>
-							Not interested?
-							<RouteLink className='link' to='/widgets-dashboard' children=' Continue' />
-						</div>
+						{currentUser ? <ProfilePage /> : <HomePage />}
 					</StyledHomePage>
 				</Card>
 			</CardAnimation>
