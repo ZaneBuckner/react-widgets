cde2dc189eef29f013249dcba1b4839e0a8ee742
zanebuckner <bucknerzane@gmail.com>
2022-02-01 23:50:05 -0600

    Add redirect link to login page
    - Remove unnecessary elements

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useAuthContext } from 'context/AuthContext';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import Page from './Page';
 import Button from 'components/shared/Button';
@@ -10,7 +10,7 @@ import { StyledRegisterPage, StyledRegisterForm } from './Pages.Styled';
 import { MdEmail as EmailIcon, MdLock as PasswordIcon } from 'react-icons/md';
 
 function RegisterPage() {
-	const { currentUser, onRegister } = useAuthContext();
+	const { onRegister } = useAuthContext();
 	const [error, setError] = useState('');
 	const [loading, setLoading] = useState(false);
 
@@ -26,24 +26,27 @@ function RegisterPage() {
 
 		setEmailError(false);
 		setPasswordError(false);
+
 		email || setEmailError('Please enter an email address.');
 		password || setPasswordError('Please enter a password.');
 		password !== passwordConfirm && setPasswordError('Passwords do not match.');
 		password.length < 6 && setPasswordError('Passwords must be at least 6 characters.');
 
-		try {
-			setError('');
-			setLoading(true);
-			await onRegister(email, password);
-		} catch {
-			setError('Failed to create account');
-			console.log(error);
-		} finally {
-			setLoading(false);
-			setEmail('');
-			setPassword('');
-			setPasswordConfirm('');
-			navigate('/');
+		if (!emailError && !passwordError) {
+			try {
+				setError('');
+				setLoading(true);
+				await onRegister(email, password);
+				navigate('/profile');
+			} catch {
+				setError('Failed to create account');
+				console.log(error);
+			} finally {
+				setLoading(false);
+				setEmail('');
+				setPassword('');
+				setPasswordConfirm('');
+			}
 		}
 	};
 
@@ -51,7 +54,6 @@ function RegisterPage() {
 		<Page>
 			<StyledRegisterPage>
 				<h1>Create Account</h1>
-				{currentUser && <h2>{currentUser.email}</h2>}
 				<StyledRegisterForm>
 					<InputField
 						required
@@ -93,6 +95,7 @@ function RegisterPage() {
 						disabled={loading}
 					/>
 				</StyledRegisterForm>
+				<Link className='redirect' to='/login' children='Already have an account?' />
 			</StyledRegisterPage>
 		</Page>
 	);
