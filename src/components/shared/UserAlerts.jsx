54cd7c6c0e02f578acb319b1c8b360e851261add
zanebuckner <bucknerzane@gmail.com>
2022-02-01 23:28:48 -0600

    Create component to return styled messages to user
    - Returns corresponding color and icon based on a variant prop that
    indicates if a message is an error, warning, or success.

---
+++ src/components/shared/UserAlerts.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+import {
+	BiErrorAlt as ErrorIcon,
+	BiError as WarningIcon,
+	BiCheckShield as SuccessIcon,
+} from 'react-icons/bi';
+
+const handleVariantColor = variant => {
+	switch (variant) {
+		case 'error':
+			return '#D92626';
+		case 'warning':
+			return '#CCB233';
+		case 'success':
+			return '#5EB34D';
+		default:
+			return '#C3C3C3';
+	}
+};
+
+const StyledAlert = styled.div`
+	display: flex;
+	align-items: center;
+	color: ${({ variant }) => handleVariantColor(variant)};
+
+	h3 {
+		font-size: 1rem;
+		font-weight: 400;
+	}
+
+	svg {
+		font-size: 1.5rem;
+		margin-right: 0.5rem;
+	}
+`;
+
+function UserAlert({ variant, message }) {
+	const variantIcon = {
+		error: <ErrorIcon />,
+		warning: <WarningIcon />,
+		success: <SuccessIcon />,
+	};
+
+	return (
+		<StyledAlert variant={variant}>
+			{variantIcon[variant]}
+			{message}
+		</StyledAlert>
+	);
+}
+
+export default UserAlert;
