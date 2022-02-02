a0ec33154308023a566f931d70a6dfbc4fccf234
zanebuckner <bucknerzane@gmail.com>
2022-02-01 23:59:38 -0600

    Create page to update user profile

---
+++ src/pages/UpdateProfilePage.jsx
@@ -0,0 +1,13 @@
+import Page from './Page';
+
+import { StyledTitle } from './Pages.Styled';
+
+function UpdateProfilePage() {
+	return (
+		<Page>
+			<StyledTitle>UPDATE PROFILE</StyledTitle>
+		</Page>
+	);
+}
+
+export default UpdateProfilePage;
