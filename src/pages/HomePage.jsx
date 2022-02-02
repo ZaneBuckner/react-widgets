f8e40f0300e599701500e54246b5d5ab0764c6ba
zanebuckner <bucknerzane@gmail.com>
2022-02-01 23:46:57 -0600

    Update home to use the page wrapper component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,47 +1,29 @@
-import { useAuthContext } from 'context/AuthContext';
 import { Link } from 'react-router-dom';
 
-import { Container } from 'globalStyles';
-import Card from 'components/shared/Card';
-import CardAnimation from 'components/shared/CardAnimation';
-
+import Page from './Page';
 import Button from 'components/shared/Button';
 import { StyledHomePage } from './Pages.Styled';
-import ProfilePage from './ProfilePage';
 
 function HomePage() {
-	const { currentUser } = useAuthContext();
-
-	const HomePage = (
-		<>
-			<h1 className='title'>Welcome To React Widgets</h1>
-			<p className='message'>
-				React is a popular open source technology developed by Facebook to build complex user
-				interfaces. To practice using this technology, I built an application full of small
-				applications - widgets.
-			</p>
-			<div className='links'>
-				<Link to='/login' children={<Button animate>Sign In</Button>} />
-				<Link to='/register' children={<Button animate>Create Account</Button>} />
-			</div>
-			<div className='redirect'>
-				Not interested?
-				<Link className='link' to='/widgets-dashboard' children=' Continue' />
-			</div>
-		</>
-	);
-
-	if (currentUser) return <ProfilePage />;
 	return (
-		<Container centerJustify>
-			<CardAnimation>
-				<Card pages>
-					<StyledHomePage className='StyledHomePage'>
-						{currentUser ? <ProfilePage /> : <HomePage />}
-					</StyledHomePage>
-				</Card>
-			</CardAnimation>
-		</Container>
+		<Page>
+			<StyledHomePage>
+				<h1 className='title'>Welcome To React Widgets</h1>
+				<p className='message'>
+					React is a popular open source technology developed by Facebook to build complex user
+					interfaces. To practice using this technology, I built an application full of small
+					applications - widgets.
+				</p>
+				<div className='links'>
+					<Link to='/login' children={<Button animate>Sign In</Button>} />
+					<Link to='/register' children={<Button animate>Create Account</Button>} />
+				</div>
+				<div className='redirect'>
+					Not interested?
+					<Link className='link' to='/widgets-dashboard' children=' Continue' />
+				</div>
+			</StyledHomePage>
+		</Page>
 	);
 }
 
