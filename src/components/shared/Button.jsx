cde81ec75d2d8e011e820028a1977f92ac82e7ae
zanebuckner <bucknerzane@gmail.com>
2022-02-02 21:33:13 -0600

    feat: update click animation and add states
    - Updated to give a button a more realistic click effect.
    - Added a disabled state that mimics the button being clicked.
    - Kept the static, no animation button.

--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -1,29 +1,37 @@
 import styled from 'styled-components';
 import { motion } from 'framer-motion';
 
-const flexCenter = `
-  display: flex;
-  justify-content: center;
-  align-items: center;
+const AnimatedWrapper = styled(motion.div)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	border: 1px solid #2e2e2e;
+	border-radius: 10px;
+	background-color: #333333;
 `;
 
 const StyledButton = styled.button`
-	${flexCenter};
+	display: flex;
+	justify-content: center;
+	align-items: center;
 	width: 3rem;
 	height: 3rem;
-	padding: 0.5rem;
-
-	border: 1px solid #2525255e;
-	border-radius: 10px;
-	background-color: transparent;
-	box-shadow: ${({ isClicked }) =>
-		!isClicked ? '2px 2px 3px #1e1e1e' : 'inset 2px 2px 3px #1e1e1e'};
-	white-space: nowrap;
+	padding: 10px;
 
 	color: #c3c3c3;
+	white-space: nowrap;
+
+	&:disabled {
+		opacity: 0.4;
+		filter: saturate(40%);
+		pointer-events: none;
+	}
 
 	a {
-		${flexCenter};
+		display: flex;
+		justify-content: center;
+		align-items: center;
 	}
 
 	svg {
@@ -31,21 +39,60 @@ const StyledButton = styled.button`
 	}
 `;
 
-function Button({ isClicked, displayed, animate, className, children, ...buttonProps }) {
-	if (animate) {
-		return (
-			<motion.div whileHover={{ scale: 1 }} whileTap={{ scale: 0.9 }}>
-				<StyledButton isClicked={isClicked} className={className} {...buttonProps}>
-					{children}
-				</StyledButton>
-			</motion.div>
-		);
-	}
-	return (
-		<StyledButton className={className} {...buttonProps}>
+const buttonVariants = {
+	initial: {
+		boxShadow: '2px 2px 3px #1E1E1E',
+	},
+	whileTap: {
+		boxShadow: 'inset 2px 2px 3px #1E1E1E',
+	},
+	transition: {
+		type: 'spring',
+	},
+	clickable: {
+		boxShadow: '2px 2px 3px #1E1E1E',
+	},
+	clicked: {
+		boxShadow: 'inset 2px 2px 3px #1E1E1E',
+	},
+};
+
+function Button({ buttonState, animate, disabled, className, children, ...props }) {
+	const StatefulButton = (
+		<AnimatedWrapper
+			variants={buttonVariants}
+			initial={buttonState ? 'clicked' : 'clickable'}
+			animate={buttonState ? 'clickable' : 'clicked'}
+			transition='transition'
+		>
+			<StyledButton className={className} {...props}>
+				{children}
+			</StyledButton>
+		</AnimatedWrapper>
+	);
+
+	const AnimatedButton = (
+		<AnimatedWrapper
+			variants={buttonVariants}
+			initial={disabled ? 'clicked' : 'clickable'}
+			whileTap='whileTap'
+			transition='transition'
+		>
+			<StyledButton disabled={disabled} className={className} {...props}>
+				{children}
+			</StyledButton>
+		</AnimatedWrapper>
+	);
+
+	const StaticButton = (
+		<StyledButton className={className} {...props}>
 			{children}
 		</StyledButton>
 	);
+
+	if (buttonState) return StatefulButton;
+	else if (animate) return AnimatedButton;
+	else return StaticButton;
 }
 
 export default Button;
