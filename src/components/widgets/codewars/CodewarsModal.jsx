f29635693b26898912e8be629327bc7a70630712
zanebuckner <bucknerzane@gmail.com>
2022-02-06 22:42:33 -0600

    refactor: reformat content for widget modal usage
    - Consolidate codewars modal into its own file.

--- src/components/widgets/codewars/CodewarsModal.jsx
+++ src/components/widgets/codewars/CodewarsModal.jsx
@@ -1,37 +1,107 @@
-import { v4 as uuidv4 } from 'uuid';
+import styled from 'styled-components';
 
-import { StyledCodewarsModal } from './Codewars.styled';
 import DifficultyBadge from './DifficultyBadge';
 
-function CodewarsModal() {
-	const kyuBadges = () => {
-		const levels = [
-			{ color: 'purple', name: '1 kyu' },
-			{ color: 'purple', name: '2 kyu' },
-			{ color: 'blue', name: '3 kyu' },
-			{ color: 'blue', name: '4 kyu' },
-			{ color: 'yellow', name: '5 kyu' },
-			{ color: 'yellow', name: '6 kyu' },
-			{ color: 'white', name: '7 kyu' },
-			{ color: 'white', name: '8 kyu' },
-		];
+const StyledIcon = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	margin: 1rem;
+`;
 
-		return levels.map(level => (
-			<DifficultyBadge key={uuidv4()} rankColor={level.color} rankName={level.name} />
-		));
-	};
+const StyledHeader = styled.h1`
+	margin-top: 1.5rem !important;
+`;
 
+const StyledBadges = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 1fr);
+	grid-template-rows: repeat(2, 1fr);
+	grid-column-gap: 1rem;
+	grid-row-gap: 1rem;
+	place-items: center;
+	width: 100%;
+`;
+
+const StyledKey = styled.div`
+	width: 100%;
+
+	ul {
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+
+		li {
+			font-size: 0.8rem;
+			font-weight: 400;
+			text-transform: uppercase;
+		}
+	}
+
+	.hardest {
+		color: #866cc7;
+	}
+
+	.hard {
+		color: #3c7ebb;
+	}
+
+	.medium {
+		color: #ecb613;
+	}
+
+	.easy {
+		color: #e6e6e6;
+	}
+`;
+
+const DifficultyBadges = () => {
+	const levels = [
+		{ id: 1, color: 'purple', name: '1 kyu' },
+		{ id: 2, color: 'purple', name: '2 kyu' },
+		{ id: 3, color: 'blue', name: '3 kyu' },
+		{ id: 4, color: 'blue', name: '4 kyu' },
+		{ id: 5, color: 'yellow', name: '5 kyu' },
+		{ id: 6, color: 'yellow', name: '6 kyu' },
+		{ id: 7, color: 'white', name: '7 kyu' },
+		{ id: 8, color: 'white', name: '8 kyu' },
+	];
+
+	return levels.map(level => (
+		<DifficultyBadge key={level.id} rankColor={level.color} rankName={level.name} />
+	));
+};
+
+const hyperlink = (
+	<a
+		href='https://www.codewars.com/dashboard'
+		target='_blank'
+		rel='noopener noreferrer'
+		className='hyperlink'
+		children='Codewars'
+	/>
+);
+
+export function About({ widgetIcon }) {
 	return (
-		<StyledCodewarsModal>
-			<div className='kyu-wrapper'>{kyuBadges()}</div>
-			<div className='key-wrapper'>
-				<h1 className='hardest'>Hardest</h1>
-				<h1 className='hard'>Hard</h1>
-				<h1 className='medium'>Medium</h1>
-				<h1 className='easy'>Easy</h1>
-			</div>
-		</StyledCodewarsModal>
+		<>
+			<StyledIcon>{widgetIcon}</StyledIcon>
+			<StyledHeader className='header'>Codewars Dashboard</StyledHeader>
+			<h2 className='subheader'>
+				{hyperlink} is a platform where developers can improve their coding prowess by solving
+				challenges at various difficulty levels.
+			</h2>
+			<StyledBadges className='body'>
+				<DifficultyBadges />
+			</StyledBadges>
+			<StyledKey>
+				<ul>
+					<li className='hardest'>Hardest</li>
+					<li className='hard'>Hard</li>
+					<li className='medium'>Medium</li>
+					<li className='easy'>Easy</li>
+				</ul>
+			</StyledKey>
+		</>
 	);
 }
-
-export default CodewarsModal;
