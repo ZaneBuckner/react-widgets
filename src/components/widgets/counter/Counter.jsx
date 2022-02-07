898fd1577b547d7fc70afe768ea1a69623175c48
zanebuckner <bucknerzane@gmail.com>
2022-02-06 20:08:55 -0600

    refactor: apply updated widget modal for counter
    - Remove previous modal implementation and replace with updated
    WidgetModal component to output the widget's AboutModal - a gif meme.

--- src/components/widgets/counter/Counter.jsx
+++ src/components/widgets/counter/Counter.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react';
 
 import Card from 'components/shared/Card';
 import CardHeader from 'components/shared/CardHeader';
-import Modal from 'components/shared/Modal';
+import WidgetModal from '../WidgetModal';
+import { About } from './CounterModal';
+
 import Button from 'components/shared/Button';
 import { StyledCounter, CountDisplay } from './Counter.styled';
 
 import CounterIcon from 'Assets/CounterIcon';
 import { FiPlus as AddIcon, FiMinus as RemoveIcon } from 'react-icons/fi';
+import confusedTravoltaGif from 'Assets/Images/confusedTravoltaGif.gif';
 
 function Counter() {
 	const [count, setCount] = useState(0);
-	const [showModal, setShowModal] = useState(false);
+	const [isAboutModal, setIsAboutModal] = useState(false);
+
+	const handleAboutToggle = () => setIsAboutModal(!isAboutModal);
 
 	const decrement = () => setCount(count - 1);
 	const increment = () => setCount(count + 1);
@@ -22,11 +27,20 @@ function Counter() {
 				name='Counter'
 				icon={<CounterIcon />}
 				widgetRef='counter'
-				setShowModal={setShowModal}
+				onAboutToggle={handleAboutToggle}
 			/>
-			<Modal showModal={showModal} setShowModal={setShowModal}>
-				<h1>Hello There</h1>
-			</Modal>
+
+			<WidgetModal
+				open={isAboutModal}
+				onClose={handleAboutToggle}
+				element={
+					<About
+						widgetIcon={<CounterIcon className='widget-icon' height={'1.5rem'} fill={'#DAB55D'} />}
+						gif={<img src={confusedTravoltaGif} alt='Confused Travolta Gif' />}
+					/>
+				}
+			/>
+
 			<StyledCounter>
 				<Button animate onClick={decrement}>
 					<RemoveIcon />
