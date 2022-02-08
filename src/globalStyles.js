91dd6ec964b762ff46d82f1a5f5cd0fefcdb9486
zanebuckner <bucknerzane@gmail.com>
2022-02-07 19:44:10 -0600

    refactor: remove unused style from global styles

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,12 +1,1 @@
 import styled from 'styled-components';
-
-export const Container = styled.div`
-	width: 100%;
-	max-width: ${({ header }) => (header ? 'none' : '1200px')};
-	margin: ${({ centerJustify }) => (centerJustify ? 'auto' : '0 auto')};
-	padding: ${({ header }) => (header ? '0 30px' : '0 50px')};
-
-	@media (max-width: 500px) {
-		padding: 0 25px;
-	}
-`;
