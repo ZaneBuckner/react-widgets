72fe4f51bc92e53785c7800beb3ed054e80cc13b
zanebuckner <bucknerzane@gmail.com>
2022-02-08 00:05:41 -0600

    refactor: remove unused styles

--- src/pages/Pages.Styled.js
+++ src/pages/Pages.Styled.js
@@ -1,128 +1,47 @@
 import styled from 'styled-components';
 
-// HOME PAGE STYLES
-export const StyledHomePage = styled.div`
-	display: grid;
-	grid-template-columns: repeat(2, 1fr);
-	grid-template-rows: repeat(4, 1fr);
-	grid-column-gap: 0px;
-	grid-row-gap: 0px;
-	place-items: center;
-	width: 100%;
-	height: 50vh;
-	padding: 1rem;
-
-	font-family: 'Roboto', serif;
-	color: #c3c3c3;
-
-	.title {
-		grid-area: 1 / 1 / 2 / 3;
-		font-size: 2rem;
-
-		font-weight: 300;
-		color: #dab55d;
-		white-space: nowrap;
-		max-width: 100%;
-	}
-
-	.message {
-		grid-area: 2 / 1 / 3 / 3;
-		max-width: 75ch;
-		height: auto;
-		font-size: 1rem;
-		font-weight: 300;
-		/* text-align: justify; */
-	}
-
-	.links {
-		grid-area: 3 / 1 / 4 / 4;
-		display: flex;
-		justify-content: space-around;
-		align-items: center;
-		width: 90%;
-		margin: 2rem;
-
-		button {
-			width: 9rem;
-			text-transform: uppercase;
-			color: #dab55d;
-		}
-	}
-
-	.redirect {
-		grid-area: 4 / 1 / 5 / 3;
-		margin-top: auto;
-
-		.link {
-			color: #dab55d;
-		}
-	}
-
-	@media only screen and (max-width: 550px) {
-		.title {
-			font-size: 1.5rem;
-		}
-
-		.links {
-			flex-direction: column;
-			button {
-				margin: 0.5rem;
-			}
-		}
-	}
-
-	@media only screen and (max-width: 400px) {
-		.title {
-			font-size: 1rem;
-		}
-
-		.message {
-			font-size: 0.8rem;
-		}
-	}
-`;
-
-// PROFILE PAGE STYLES
-export const StyledProfilePage = styled.div`
-	width: 100%;
-	height: 50vh;
-
-	font-family: 'Roboto', serif;
-	color: #c3c3c3;
-`;
-
-// LOGIN PAGE STYLES
-export const StyledLoginPage = styled.div`
+export const StyledErrorMessage = styled.h3`
 	display: flex;
-	flex-direction: column;
 	align-items: center;
-	width: 100%;
-	height: 50vh;
-	place-items: center;
+`;
 
-	font-family: 'Roboto', serif;
-	color: #c3c3c3;
+export const StyledSuccessMessage = styled.div`
+	display: flex;
+	align-items: center;
+	color: #5eb34d;
 
-	h1 {
-		font-size: 2rem;
-		font-weight: 300;
-		color: #dab55d;
-		white-space: nowrap;
+	h3 {
+		font-size: 1rem;
+		font-weight: 400;
 	}
 
-	.user-forgets {
-		margin-top: auto;
-
-		&:hover {
-			color: #dab55d;
-		}
+	svg {
+		font-size: 1.5rem;
+		margin-right: 0.5rem;
 	}
 `;
 
 export const StyledLoginForm = styled.div`
+	grid-area: 3 / 1 / 4 / 2;
 	display: grid;
 	grid-template-columns: 1fr;
-	grid-template-rows: 1fr 1fr 2fr;
+	grid-template-rows: 1fr 1fr 1fr;
+	grid-row-gap: 2rem;
+	place-items: center;
+	width: 100%;
+
+	.submit-btn {
+		width: 9rem;
+		text-transform: uppercase;
+		color: #dab55d;
+	}
+`;
+
+export const StyledRegisterForm = styled.div`
+	grid-area: 3 / 1 / 5 / 2;
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: 1fr 1fr 1fr 1fr;
 	grid-row-gap: 2rem;
 	place-items: center;
 	width: 100%;
@@ -135,34 +54,13 @@ export const StyledLoginForm = styled.div`
 	}
 `;
 
-// REGISTER PAGE STYLES
-export const StyledRegisterPage = styled.div`
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	width: 100%;
-	height: 50vh;
-	place-items: center;
-
-	font-family: 'Roboto', serif;
-	color: #c3c3c3;
-
-	h1 {
-		font-size: 2rem;
-		font-weight: 300;
-		color: #dab55d;
-		white-space: nowrap;
-	}
-`;
-
-export const StyledRegisterForm = styled.div`
+export const StyledResetPasswordForm = styled.div`
+	grid-area: 3 / 1 / 5 / 2;
 	display: grid;
 	grid-template-columns: 1fr;
-	grid-template-rows: 1fr 1fr 1fr 2fr;
-	grid-row-gap: 2rem;
+	grid-template-rows: 1fr 2fr;
 	place-items: center;
 	width: 100%;
-	margin: auto 0;
 
 	.submit-btn {
 		width: 9rem;
