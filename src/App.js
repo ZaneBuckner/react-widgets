0eeb8af77afb7d1d8eb75fc2411b08897a978fea
zanebuckner <bucknerzane@gmail.com>
2022-02-09 16:09:06 -0600

    feat: implement private route and add new pages
    - Private route was setup to block access to profile and updateProfile
    pages if users do not have a profile/account.
    - Remove loading state as it was implemented from the AuthContext file.
    - Included new pages.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import PrivateRoute from 'components/shared/PrivateRoute';
 
 import { AuthContextProvider } from 'context/AuthContext';
 import { WidgetContextProvider } from 'context/WidgetContext';
-import { useAuthContext } from 'context/AuthContext';
 
 import HomePage from 'pages/HomePage';
 import LoginPage from 'pages/LoginPage';
+import PasswordResetPage from 'pages/PasswordResetPage';
+import UpdateProfilePage from 'pages/UpdateProfilePage';
 import RegisterPage from 'pages/RegisterPage';
+import ProfilePage from 'pages/ProfilePage';
+import PageNotFound from 'pages/PageNotFound';
 import Header from './components/header/Header';
 import Toolbar from './components/toolbar/Toolbar';
 import Widgets from './components/widgets/Widgets';
 import Footer from 'components/footer/Footer';
 
 function App() {
-	const { loading } = useAuthContext();
-
 	return (
 		<AuthContextProvider>
 			<Router>
 				<Header />
 				<Routes>
+					<Route path='*' element={<PageNotFound />} />
 					<Route exact path='/' element={<HomePage />} />
 					<Route
-						path='/widgets-dashboard'
+						path='/profile'
+						element={
+							<PrivateRoute>
+								<ProfilePage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path='/profile-update'
+						element={
+							<PrivateRoute>
+								<UpdateProfilePage />
+							</PrivateRoute>
+						}
+					/>
+					<Route
+						path='/widgets'
 						element={
 							<WidgetContextProvider>
 								<Toolbar />
@@ -30,8 +49,9 @@ function App() {
 							</WidgetContextProvider>
 						}
 					/>
-					{!loading && <Route path='/login' element={<LoginPage />} />}
-					{!loading && <Route path='/register' element={<RegisterPage />} />}
+					<Route path='/login' element={<LoginPage />} />
+					<Route path='/register' element={<RegisterPage />} />
+					<Route path='/password-reset' element={<PasswordResetPage />} />
 				</Routes>
 				<Footer />
 			</Router>
