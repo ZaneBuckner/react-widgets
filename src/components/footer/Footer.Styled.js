a421d21bf9ec73c2a46ce5c2dfb770a6e760af88
zanebuckner <bucknerzane@gmail.com>
2022-02-13 01:26:21 -0600

    fix: set margin to keep footer at bottom
    - margin-top: auto

--- src/components/footer/Footer.Styled.js
+++ src/components/footer/Footer.Styled.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
 	width: 100%;
-	margin-top: 2rem;
+	margin-top: auto;
 
 	box-shadow: inset 0 6px 13px -4px rgb(0 0 0 / 48%);
 	background-color: #2b2b2b;
