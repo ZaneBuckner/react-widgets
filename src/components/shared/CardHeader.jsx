26da47b2a3b4f9ff8000324806c250c7b0a2c594
zanebuckner <bucknerzane@gmail.com>
2022-02-12 19:47:22 -0600

    feat: update component features and cleanup
    - Repositioned styles to the end of the file.
    - Implemented the reusable search component for widgets to use.
    - Removed unused styles.

--- src/components/shared/CardHeader.jsx
+++ src/components/shared/CardHeader.jsx
@@ -1,10 +1,44 @@
-import { useContext } from 'react';
-import { WidgetContext } from 'context/WidgetContext';
+import { useWidgetContext } from 'context/WidgetContext';
 import styled from 'styled-components';
 
 import { AiFillInfoCircle as InfoIcon, AiFillCloseCircle as CancelIcon } from 'react-icons/ai';
 
+export default function CardHeader({
+	name,
+	icon,
+	widgetRef,
+	widgetSearch,
+	onAboutToggle,
+	onUtilityToggle,
+}) {
+	const { toggleDisplay } = useWidgetContext();
+
+	return (
+		<StyledCardHeader>
+			{icon}
+			<h1 className='widget-title'>{name}</h1>
+			<StyledIcons>
+				{onUtilityToggle && onUtilityToggle}
+				{widgetSearch && widgetSearch}
+				{onAboutToggle && (
+					<InfoIcon
+						className='action-icons'
+						aria-label='Open Widget Modal'
+						onClick={onAboutToggle}
+					/>
+				)}
+				<CancelIcon
+					className='action-icons'
+					aria-label='Close Widget'
+					onClick={() => toggleDisplay(widgetRef)}
+				/>
+			</StyledIcons>
+		</StyledCardHeader>
+	);
+}
+
 const StyledCardHeader = styled.div`
+	position: relative;
 	display: flex;
 	align-items: center;
 	width: 100%;
@@ -12,7 +46,6 @@ const StyledCardHeader = styled.div`
 	margin-bottom: 0.5rem;
 
 	font-family: 'Roboto', serif;
-	opacity: 70%;
 
 	svg {
 		width: auto;
@@ -25,10 +58,6 @@ const StyledCardHeader = styled.div`
 		font-size: 0.8rem;
 		font-weight: 400;
 		color: #dab55d;
-
-		@media (max-width: 350px) {
-			display: none;
-		}
 	}
 
 	@media (max-width: 500px) {
@@ -36,7 +65,7 @@ const StyledCardHeader = styled.div`
 	}
 `;
 
-const StyledCardHeaderActions = styled.div`
+const StyledIcons = styled.div`
 	display: flex;
 	justify-content: center;
 	height: 100%;
@@ -54,80 +83,11 @@ const StyledCardHeaderActions = styled.div`
 
 		background-color: rgba(255, 255, 255, 0.1);
 		border-radius: 5px;
-
-		::placeholder {
-			font: inherit;
-			color: #c3c3c3;
-		}
 	}
 
 	.action-icons {
 		cursor: pointer;
 		height: 100%;
-		margin-left: 0.2rem;
+		margin-left: 0.3rem;
 	}
 `;
-
-function CardHeader({
-	name,
-	icon,
-	placeholder,
-	widgetRef,
-	setUserInput,
-	setShowModal,
-	utilityModal,
-}) {
-	const { toggleDisplay } = useContext(WidgetContext);
-
-	const handleUserInput = e => e.key === 'Enter' && setUserInput(e.target.value);
-
-	// OPTIONAL: GIVES WIDGET SEARCH INPUT
-	const widgetActionInput = () => {
-		return (
-			<input
-				className='action-input'
-				type='text'
-				placeholder={placeholder}
-				spellCheck='false'
-				onKeyPress={handleUserInput}
-			/>
-		);
-	};
-
-	// OPTIONAL: GIVES WIDGET A DEFAULT POPUP MODAL
-	const widgetActionModal = () => {
-		return (
-			<InfoIcon
-				className='action-icons'
-				aria-label='Open Widget Modal'
-				onClick={() => setShowModal(prev => !prev)}
-			/>
-		);
-	};
-
-	// REQUIRED: CLOSES WIDGET
-	const widgetActionClose = () => {
-		return (
-			<CancelIcon
-				className='action-icons'
-				aria-label='Close Widget'
-				onClick={() => toggleDisplay(widgetRef)}
-			/>
-		);
-	};
-
-	return (
-		<StyledCardHeader>
-			{icon}
-			<h1 className='widget-title'>{name}</h1>
-			<StyledCardHeaderActions>
-				{setUserInput && widgetActionInput()}
-				{utilityModal}
-				{setShowModal && widgetActionModal()}
-				{widgetActionClose()}
-			</StyledCardHeaderActions>
-		</StyledCardHeader>
-	);
-}
-
-export default CardHeader;
