b8e4ed98cc6a25fb0ef9c72a57ba788b4517a3ae
zanebuckner <bucknerzane@gmail.com>
2022-02-15 22:18:52 -0600

    feat: create reusable avatar component
    - Modified MUI Avatar component.
    - Included are buttons to browse and submit chosen image.

---
+++ src/components/shared/Avatar.jsx
@@ -0,0 +1,72 @@
+import { styled } from '@mui/material/styles';
+
+import Button from './Button';
+import Avatar from '@mui/material/Avatar';
+
+import { FaUserCircle as DefaultIcon, FaImages as ImageFileIcon } from 'react-icons/fa';
+import { IoMdCloudDone as UploadIcon } from 'react-icons/io';
+
+export function UserAvatar({ size, variant, src }) {
+	return (
+		<StyledAvatar size={size} variant={variant} src={src}>
+			<DefaultIcon />
+		</StyledAvatar>
+	);
+}
+
+export function UserAvatarBrowse({ onImageSelect }) {
+	return (
+		<Button animate size='auto' variant='combo' icon={<ImageFileIcon />}>
+			<label htmlFor='input'>
+				<input
+					id='input'
+					type='file'
+					accept='image/*'
+					style={{ display: 'none' }}
+					onChange={onImageSelect}
+				/>
+				<span style={{ cursor: 'pointer' }}>Browse...</span>
+			</label>
+		</Button>
+	);
+}
+
+export function UserAvatarUpload({ onImageUpload, loading }) {
+	return (
+		<Button
+			animate
+			size='auto'
+			variant='combo'
+			icon={<UploadIcon />}
+			children='Save Avatar'
+			disabled={loading}
+			onClick={onImageUpload}
+		/>
+	);
+}
+
+const StyledAvatar = styled(Avatar)`
+	width: 100%;
+	height: 100%;
+	background: transparent;
+	color: #dab55d;
+
+	svg {
+		width: 100%;
+		height: 100%;
+	}
+
+	${({ size }) =>
+		size === 'small' &&
+		`
+      width: 4rem;
+      height: 4rem;
+  `};
+
+	${({ size }) =>
+		size === 'medium' &&
+		`
+      width: 6rem;
+      height: 6rem;
+  `};
+`;
