4233f44e71706aae098fb8b9791ceb16a4d2ecdd
zanebuckner <bucknerzane@gmail.com>
2022-02-16 17:03:59 -0600

    feat: create methods to update user account
    - UpdateEmail, UpdatePassword, and UsernameUpdate methods added.
    - The file was also refactored for readability.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,46 +1,40 @@
 import { useState, useEffect, useContext, createContext } from 'react';
+import { auth } from '../firebase'; // FIREBASE AUTH INSTANCE
 import {
-	createUserWithEmailAndPassword,
-	// updateProfile,
-	onAuthStateChanged,
-	signInWithEmailAndPassword,
 	signOut,
+	updateEmail,
+	updateProfile,
+	updatePassword,
+	onAuthStateChanged,
 	sendPasswordResetEmail,
+	signInWithEmailAndPassword,
+	createUserWithEmailAndPassword,
 } from 'firebase/auth';
 
-import { auth } from '../firebase';
-
-// CREATE AUTH CONTEXT
+// AUTH CONTEXT INSTANCE
 const AuthContext = createContext({});
 
-// RETURNS AUTH CONTEXT DATA
+// AUTH CONTEXT "HOOK"
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthContextProvider = ({ children }) => {
 	const [currentUser, setCurrentUser] = useState(null);
 	const [loading, setLoading] = useState(true);
 
-	useEffect(() => !loading && currentUser && console.log(currentUser), [currentUser, loading]);
+	useEffect(() => {
+		!loading && currentUser && console.log(currentUser);
+	}, [currentUser, loading]);
 
-	// LOG IN
-	const handleLogin = (email, password) => {
-		return signInWithEmailAndPassword(auth, email, password);
-	};
-
-	// LOG OUT
+	// FIREBASE AUTHENTICATION METHODS
+	const handleLogin = (email, password) => signInWithEmailAndPassword(auth, email, password);
 	const handleLogout = () => signOut(auth);
+	const handleRegister = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+	const handleEmailUpdate = newEmail => updateEmail(currentUser, newEmail);
+	const handlePasswordReset = email => sendPasswordResetEmail(auth, email);
+	const handlePasswordUpdate = newPassword => updatePassword(currentUser, newPassword);
+	const handleUsernameUpdate = newName => updateProfile(currentUser, { displayName: newName });
 
-	// NEW ACCOUNT
-	const handleRegister = (email, password) => {
-		return createUserWithEmailAndPassword(auth, email, password);
-	};
-
-	// USER FORGOT PASSWORD
-	const handlePasswordReset = email => {
-		return sendPasswordResetEmail(auth, email);
-	};
-
-	// WHEN A USER SIGNS IN OR LOGS OUT
+	// FIREBASE AUTH OBSERVER => WHEN USER STATE CHANGES (LOGIN & LOGOUT)
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, user => {
 			setCurrentUser(user);
@@ -52,12 +46,14 @@ export const AuthContextProvider = ({ children }) => {
 	return (
 		<AuthContext.Provider
 			value={{
-				loading,
 				currentUser,
 				onLogin: handleLogin,
 				onLogout: handleLogout,
 				onRegister: handleRegister,
+				onEmailUpdate: handleEmailUpdate,
 				onPasswordReset: handlePasswordReset,
+				onPasswordUpdate: handlePasswordUpdate,
+				onUsernameUpdate: handleUsernameUpdate,
 			}}
 		>
 			{!loading && children}
