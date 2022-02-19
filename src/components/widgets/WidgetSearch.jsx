73b61693b9c762d70851a4e1e356a0aed7436d19
zanebuckner <bucknerzane@gmail.com>
2022-02-18 18:30:56 -0600

    refactor: modify input styles and cleanup unused

--- src/components/widgets/WidgetSearch.jsx
+++ src/components/widgets/WidgetSearch.jsx
@@ -4,13 +4,12 @@ import { HiSearchCircle as SearchIcon } from 'react-icons/hi';
 import { AiOutlineClose as CloseIcon } from 'react-icons/ai';
 
 export default function WidgetSearch({ open, onToggle, onSubmit, placeholder }) {
-	return open ? (
+	if (!open) return <StyledSearchIcon className='action-icons' onClick={onToggle} />;
+	return (
 		<SearchContainer>
 			<SearchInput type='text' placeholder={placeholder} spellCheck='false' onKeyPress={onSubmit} />
-			<CloseIcon className='close-icon' onClick={onToggle} />
+			<StyledCloseIcon onClick={onToggle} />
 		</SearchContainer>
-	) : (
-		<StyledSearchIcon className='action-icons' onClick={onToggle} />
 	);
 }
 
@@ -21,19 +20,11 @@ const SearchContainer = styled.div`
 	display: flex;
 	align-items: center;
 	width: 100%;
-	height: 120%;
+	height: 125%;
 
-	border-radius: 5px;
+	border-radius: 10px;
 	background-color: rgba(0, 0, 0, 0.4);
 	backdrop-filter: blur(8px) saturate(130%);
-
-	.close-icon {
-		position: absolute;
-		right: 0;
-		color: #c3c3c3;
-		font-size: 0.8rem;
-		cursor: pointer;
-	}
 `;
 
 const SearchInput = styled.input`
@@ -42,12 +33,20 @@ const SearchInput = styled.input`
 	padding: 0 0.5rem;
 
 	font-family: 'Montserrat', serif;
-	font-weight: 500;
+	font-size: 0.9rem;
+	font-weight: 400;
 	color: #c3c3c3;
 
-	border-radius: 5px;
 	background-color: rgba(70, 70, 70, 0.3);
 	backdrop-filter: blur(8px) saturate(130%);
+`;
+
+const StyledCloseIcon = styled(CloseIcon)`
+	position: absolute;
+	right: 0;
+	height: 100%;
+	margin-right: 0.3rem;
+	cursor: pointer;
 `;
 
 const StyledSearchIcon = styled(SearchIcon)`
