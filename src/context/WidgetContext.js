cb198dbdb23a7011fcbf525e02d9eaec2859683d
zanebuckner <bucknerzane@gmail.com>
2022-03-14 17:15:05 -0500

    feat: integrate user data with widget display
    - For authenticated users, the state of widgets opened or closed will
    now follow their account.

--- src/context/WidgetContext.js
+++ src/context/WidgetContext.js
@@ -1,4 +1,5 @@
-import { useState, createContext, useContext } from 'react';
+import { useState, useEffect, createContext, useContext } from 'react';
+import { useAuthContext } from './AuthContext';
 import { v4 as uuidv4 } from 'uuid';
 
 const WidgetContext = createContext({}); // WIDGET CONTEXT INSTANCE
@@ -44,20 +45,27 @@ const defaultWidgets = [
 ];
 
 export const WidgetContextProvider = ({ children }) => {
+	const { currentUser, userData, onDocumentUpdate } = useAuthContext();
 	const [widgets, setWidgets] = useState(defaultWidgets);
 
+	// UPDATE WIDGET DISPLAY STATE
 	const toggleDisplay = widgetRef => {
-		// Create shallow copy. Define target widget index. Toggle widget display property. Set new widgets object.
-		let updatedWidgets = [...widgets];
-		let index = updatedWidgets.findIndex(widget => widget.widgetRef === widgetRef);
-		updatedWidgets[index] = { ...updatedWidgets[index], display: !updatedWidgets[index].display };
-		setWidgets(updatedWidgets);
+		let updatedWidgets = [...widgets]; // SHALLOW COPY TO MUTATE
+		let index = updatedWidgets.findIndex(widget => widget.widgetRef === widgetRef); // TARGET WIDGET'S INDEX
+		updatedWidgets[index] = { ...updatedWidgets[index], display: !updatedWidgets[index].display }; // TOGGLE TARGET WIDGET'S DISPLAY VALUE
+		currentUser ? onDocumentUpdate({ widgets: updatedWidgets }) : setWidgets(updatedWidgets); // UPDATE DOCUMENT || STATE
 	};
 
-	// RETURNS CURRENT WIDGET DISPLAY VALUE
+	// READ WIDGET DISPLAY STATE
 	const currentDisplay = widgetRef => {
 		return widgets.filter(widget => widget.widgetRef === widgetRef)[0].display;
 	};
+
+	// SET AUTH USER WIDGETS => ON MOUNT & WHEN DATA UPDATES
+	useEffect(() => userData && setWidgets(userData?.widgets || defaultWidgets), [userData]);
+
+	// SET NON-AUTH USER WIDGETS AS DEFAULT => WHEN USER STATE CHANGES
+	useEffect(() => !currentUser && setWidgets(defaultWidgets), [currentUser]);
 
 	return (
 		<WidgetContext.Provider
